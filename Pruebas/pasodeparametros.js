dcd23bcc993c85605b36e03f523e2aaefdf61cfa
jgozalo <javinl7@hotmail.com>
2022-02-22 12:58:24 +0100

    Cambio en el paso de parametros

--- Pruebas/pasodeparametros.js
+++ Pruebas/pasodeparametros.js
@@ -4,40 +4,40 @@
 function suma(x,y) {
     //-- devolver la suma
     return x+y;
-  }
+}
   
   //-- Recibe un parámetro y lo imprime por la consola
-  function mensaje(msg) {
+function mensaje(msg) {
     console.log(msg);
-  }
+}
   
   //-- Funcion que no recibe parametros
-  function saluda() {
+function saluda() {
       mensaje("HOLA!!");
-  }
+}
   
   //-- Funcion que recibe una funcion como parametro
   //-- y simplemente la llama 
-  function call(func) {
+function call(func) {
     console.log("--> Funcion recibida");
   
     //-- Llamar a la funcion pasada como argumento
     func();
-  }
+}
   
   //-- Llamar a suma
-  let a = suma(2,3);
+let a = suma(2,3);
   
   //-- Probando la funcione mensaje
-  mensaje("Prueba")
-  mensaje(a);
+mensaje("Prueba")
+mensaje(a);
   
   //-- Probando la funcion call
-  call(saluda);
+call(saluda);
   
   //-- Se le pasa como parametro una funcion
   //-- que se define dentro de los parmatros, vez de 
   //-- fuera
-  call( () => {
+call( () => {
     mensaje("HOLI!!")
-  });
+});

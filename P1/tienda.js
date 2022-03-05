5ccb359467f56ae20f457337770a8ce5b9e18491
jgozalo <javinl7@hotmail.com>
2022-03-05 16:34:46 +0100

    Prueba peticion correcta

--- P1/tienda.js
+++ P1/tienda.js
@@ -3,38 +3,6 @@ const fs = require('fs');
 
 const PUERTO = 9090;
 
-//-- Texto HTML de la página principal
-const pagina_main = `
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mi tienda</title>
-</head>
-<body style="background-color: lightblue">
-    <h1 style="color: green">MI TIENDA</h1>
-</body>
-</html>
-`
-
-//-- Texto HTML de la página de error
-const pagina_error = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mi tienda</title>
-</head>
-<body style="background-color: red">
-    <h1 style="color: white">ERROR!!!!</h1>
-</body>
-</html>
-`
 
 function print_info_req(req) {
 
@@ -63,12 +31,9 @@ const server = http.createServer((req, res)=>{
 
     //print_info_req(req);
 
-    
-
     //-- Valores de la respuesta por defecto
     let code = 200;
     let code_msg = "OK";
-    let page = pagina_main;
 
     //-- Analizar el recurso
     //-- Construir el objeto url con la url de la solicitud
@@ -76,21 +41,28 @@ const server = http.createServer((req, res)=>{
     console.log("URL (del recurso solicitado): " + url.href)
     console.log("Ruta: ",url.pathname);
 
-    //-- Cualquier recurso que no sea la página principal
-    //-- genera un error
-    if (url.pathname != '/') {
-        code = 404;
-        code_msg = "Not Found";
-        page = pagina_error;
+    
+    if (url.pathname = '/') {
+        petition = "./main.html"
     }
 
     //-- Generar la respusta en función de las variables
     //-- code, code_msg y page
     res.statusCode = code;
     res.statusMessage = code_msg;
-    res.setHeader('Content-Type','text/html');
-    res.write(page);
-    res.end();
+    //-- Lectura asincrona de los recursos a mostrar en la pagina
+    fs.readFile(petition, (err, data) => {
+        if (err) {
+            console.log("NO");
+            //-- Para que deje de buscar el recurso y vuelva a esperar peticiones
+            return res.end();
+        }
+        console.log("SI");
+        //-- Escribo la cabecera del mensaje y muestro la pagina solicitada
+        res.setHeader('Content-Type','text/html')
+        res.write(data);
+        res.end();
+    });
 });
 
 server.listen(PUERTO);

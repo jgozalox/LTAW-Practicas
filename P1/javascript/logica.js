63f0ba5428f305d26ff237df9d729e33d76151b4
jgozalo <javinl7@hotmail.com>
2022-03-07 12:15:33 +0100

    Ajustes en nomenclaturas

--- P1/javascript/logica.js
+++ P1/javascript/logica.js
@@ -1,3 +1,11 @@
-document.getElementById("golden-manzana").onclick = function () {
+document.getElementById("goldensupreme").onclick = function () {
+    location.href = "http://127.0.0.1:9090/error.html"; 
+};
+
+document.getElementById("reddelicious").onclick = function () {
+    location.href = "http://127.0.0.1:9090/error.html"; 
+};
+
+document.getElementById("grannysmith").onclick = function () {
     location.href = "http://127.0.0.1:9090/error.html"; 
 };
